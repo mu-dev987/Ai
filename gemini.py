31fbf291e1ca7febdb580f9020654699dd01827a
mu-dev987 <mamunshahid987@gmail.com>
2026-07-04 21:32:54 +0500

    Ready for Deployment

--- gemini.py
+++ gemini.py
@@ -3,7 +3,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate 
 from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
-from API import GEMINI_API_KEY
 os.environ['GEMINI_API_KEY'] = st.Secrets["MY_API_KEY"]
 output_cleaner = StrOutputParser()
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=1)
