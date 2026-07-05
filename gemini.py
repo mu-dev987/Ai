98e39f474666a340c96679961fc46bbc40e38b18
mu-dev987 <mamunshahid987@gmail.com>
2026-07-05 16:00:26 +0500

    Core Code

--- gemini.py
+++ gemini.py
@@ -1,30 +1,33 @@
 import os 
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate 
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
-os.environ['GEMINI_API_KEY'] = st.secrets["YOUR_API_KEY"]
+os.environ['GEMINI_API_KEY'] = st.secrets["MY_API_KEY"]
 output_cleaner = StrOutputParser()
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.7)
-translation_prompt = ChatPromptTemplate.from_messages([
-    ("system","You are a urdu-english expert. You are to take any line written in english and output the urdu trnaslation in the same script as english "),
-    ("human","{text}")
-])
+# translation_prompt = ChatPromptTemplate.from_messages([
+#     ("system","You are a urdu-english expert. You are to take any line written in english and output the urdu trnaslation in the same script as english "),
+#     ("human","{text}")
+# ])
 answer_prompt = ChatPromptTemplate.from_messages([
-    ("system","you are to take into account the text and produce an appropiate response in the urdu language with the english script. Make sure that the response is only in the english script."),
+    ("system","you are to take into account the text and produce an appropiate response in the urdu language with the english script. Make sure that the response is only in the english script. Make the response adequately matching the prompt of the user and try to be precise in giving the answer. if it is a logical or memory question be precise and dont hallucinate , but if its another type of question you are allowd to be creative, but do not go away from the actual prommpt"),
     ("human","{text}")
 ])
-chain = translation_prompt | llm | output_cleaner | {"text": lambda x: x} | answer_prompt | llm | output_cleaner
+# translation_prompt | llm | output_cleaner | {"text": lambda x: x} | 
+chain = answer_prompt | llm | output_cleaner
 st.title(":red[Urdu AI Assistant]")
 input = st.text_area("",placeholder="What can I asssit you with today?")
+response_area = st.empty()
 if st.button("ANSWER"):
+    response_area = st.empty()
     if input == "":
-        st.write("WRITE SOMETHING!")
+        response_area.write("WRITE SOMETHING!")
     else:
         with st.spinner("Thinking..."):
             try:
                 response = chain.invoke(input)
-                st.write(response)
+                response_area.write(response)
             except Exception as e:
                 if "429" in str(e):
                     st.error( "AW SNAP! our Ai tokens are finished :(")
@@ -32,4 +35,4 @@ if st.button("ANSWER"):
                     st.error(f"OOPS! something went wrong {type(e).__name__}")
         
 else:
-    st.write("")
+    response_area.write("")
